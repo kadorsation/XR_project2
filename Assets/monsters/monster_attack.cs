c9ff4a21f433c5aedc7413faded2f285ce446465
yide <derek0989518150@gmail.com>
2021-04-28 19:58:03 +0800

    monster animation

---
+++ Assets/monsters/monster_attack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monster_attack : MonoBehaviour
+{
+    // Start is called before the first frame update
+	private Animator animator;
+	bool attack = true;
+    void Start () {
+        animator = GetComponent<Animator>();
+        //Debug.Log("attack!");
+    }
+ 	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+        	Debug.Log("attack: " + attack);
+            animator.SetBool("attack", attack);
+            attack = !attack;
+        }
+    }
+}
