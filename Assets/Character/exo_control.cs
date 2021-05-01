27167902b278c5a668eeed185b5eeaa9d073e03f
yide <derek0989518150@gmail.com>
2021-05-01 16:14:08 +0800

    add character

---
+++ Assets/Character/exo_control.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exo_control : MonoBehaviour
+{
+    // Start is called before the first frame update
+	private Animator animator;
+    void Start () {
+        animator = GetComponent<Animator>();
+        //Debug.Log("attack!");
+    }
+ 	void Update () {
+ 		animator.SetBool("shooting", false);
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+        	//shoot
+        	Debug.Log("shoot");
+            animator.SetBool("shooting", true);
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+        	//stand
+        	Debug.Log("stand");
+            animator.SetBool("running", false);
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+        	//run
+        	Debug.Log("run");
+            animator.SetBool("running", true);
+        }
+    }
+}
