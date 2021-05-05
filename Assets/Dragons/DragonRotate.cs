b6d83ab530f0501e38877bf82450f22f32f38ac3
a3102a123 <a3102a123@gmail.com>
2021-05-06 01:22:01 +0800

    dragon rotate

---
+++ Assets/Dragons/DragonRotate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float fRotateSpeed = 30;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PositionControl();
+    }
+
+	void PositionControl()
+	{
+
+		if (Input.GetKey(KeyCode.D)) {
+			transform.Rotate(Vector3.up * Time.deltaTime * fRotateSpeed);
+		}
+		else if (Input.GetKey(KeyCode.A)) {
+			transform.Rotate(Vector3.down * Time.deltaTime * fRotateSpeed);
+		}
+	}
+}
