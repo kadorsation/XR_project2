9c02ab2986b3a887471eb4156f2c8d7b2c6ba208
a3102a123 <a3102a123@gmail.com>
2021-05-06 04:06:46 +0800

    skill

--- Assets/Scenes/MyScenes/Dragon/DragonAttack.cs
+++ Assets/Scenes/MyScenes/Dragon/DragonAttack.cs
@@ -19,4 +19,26 @@ public class DragonAttack : MonoBehaviour
 
         //Invoke(DestroyPS, 2.0f);
     }
+    public void Fireball()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Fireball");
+            animator.SetInteger("dragon_motion", 1);
+            GameObject fireball = Instantiate(projectile,
+                new Vector3(mouthEnd.transform.position.x, mouthEnd.transform.position.y, mouthEnd.transform.position.z),
+                transform.rotation) as GameObject;
+            Rigidbody rb = fireball.GetComponent<Rigidbody>();
+            rb.velocity = transform.forward * projectileSpeed;
+        }
+    }
+
+    public void AttackFly()
+    {
+        if (Input.GetKey(KeyCode.J))
+        {
+            Debug.Log("attackFly");
+            animator.SetInteger("dragon_motion", 3);
+        }
+    }
 }
--- Assets/UI/Scripts/skill_cd.cs
+++ Assets/UI/Scripts/skill_cd.cs
@@ -2,19 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Threading;
 public class skill_cd : MonoBehaviour
 {
     public int time_set;
     int time_temp;
     public Text time_UI;
     public GameObject cover;
+    public int skill_id;
+    public GameObject projectile;
+    public float projectileSpeed;
+    public GameObject dragon;
+    public GameObject mouthEnd;
+    public GameObject wingEnd1;
+    public GameObject wingEnd2;
+    private GameObject player;
+
+    private Animator animator;
+    private WaitForSeconds shortduration = new WaitForSeconds(1);
     void Start()
     {
+        animator = dragon.GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
     //call into_cd function when dragon use skill
     public void into_cd()
     {
-        Debug.Log("use skill!");
+        switch (skill_id)
+        {
+            case 1:
+                MouthAttack();
+                break;
+            case 2:
+                AttackFly();
+                break;
+            case 3:
+                Fireball();
+                break;
+        }
         GetComponent<Button>().interactable = false;
         //unable skill
         time_temp = time_set;
@@ -34,5 +59,48 @@ public class skill_cd : MonoBehaviour
             GetComponent<Button>().interactable = true;
             //enable skill
         }
+    }
+    public void MouthAttack()
+    {
+        Debug.Log("attackMouth");
+        animator.Play("attackMouth");
+    }
+
+    public void Fireball()
+    {
+        Debug.Log("Fireball");
+        animator.Play("attackFlame");
+        //animator.SetInteger("dragon_motion", 1);
+        //Thread.Sleep(1000);
+        StartCoroutine (WaitEffect());
+        
+        
+    }
+
+    public void AttackFly()
+    {
+        Debug.Log("attackFly");
+        animator.Play("attackFly"); 
+        GameObject wind1 = Instantiate(projectile,
+                 new Vector3(wingEnd1.transform.position.x, wingEnd1.transform.position.y, wingEnd1.transform.position.z),
+                 transform.rotation) as GameObject;
+        GameObject wind2 = Instantiate(projectile,
+                 new Vector3(wingEnd2.transform.position.x, wingEnd2.transform.position.y, wingEnd2.transform.position.z),
+                 transform.rotation) as GameObject;
+        Rigidbody rb1 = wind1.GetComponent<Rigidbody>();
+        rb1.velocity = (player.transform.position- wingEnd1.transform.position) * projectileSpeed;
+
+        Rigidbody rb2 = wind2.GetComponent<Rigidbody>();
+        rb2.velocity = (player.transform.position - wingEnd2.transform.position) * projectileSpeed;
+    }
+
+    private IEnumerator WaitEffect()
+    {
+        yield return shortduration;
+        GameObject fireball = Instantiate(projectile,
+                new Vector3(mouthEnd.transform.position.x, mouthEnd.transform.position.y, mouthEnd.transform.position.z),
+                transform.rotation) as GameObject;
+        Rigidbody rb = fireball.GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * projectileSpeed;
     }
 }
